bc6ab2cae8d6f4b6413d02ae715835a35bbb6b41
sevilledev <sevillerasulova@gmail.com>
2023-02-10 03:04:34 -0800

    Edited calc buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Calc1 } from './components/calc1'
 import { Calc2 } from './components/calc2'
 import { Routes, Route } from 'react-router-dom'
+import {Navbar} from './components/navbar'
 
 
 export const App = () => {
 	return (
-		<Routes>
-			<Route path="/calc1" element={<Calc1 />} />
-			<Route path="/calc2" element={<Calc2 />} />
-		</Routes>
+		<>
+			<Navbar />
+			<Routes>
+                <Route path='/*' element={<Calc2 />} />
+                <Route exact path='/' element={<Calc2 />} />
+				<Route path="/calc1" element={<Calc1 />} />
+				<Route path="/calc2" element={<Calc2 />} />
+			</Routes>
+		</>
 	)
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,65 @@
+import { NavLink, Link, useLocation } from 'react-router-dom'
+
+
+const sty = {
+    // navbar: {
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     position: 'fixed',
+    //     width: '100vw',
+    //     height: 50,
+    //     backdropFilter: 'saturate(180%) blur(20px)',
+    //     backgroundColor: 'rgba(255, 255, 255, 0.72)',
+    //     zIndex: 1,
+    //     border: '1px solid #CECECF',
+    //     borderWidth: 0,
+    //     borderBottomWidth: '1px'
+    // },
+    navbarInner: {
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    nav: {
+        textDecoration: 'none',
+        color: 'var(--primary-tint)',
+        margin: '0 7px',
+        fontSize: 20,
+        fontVariationSettings: '"wght" 500',
+        borderRadius: '10px',
+    },
+    activeNav: {
+        textDecoration: 'none',
+        color: 'var(--tertiary-label)',
+        margin: '0 7px',
+        fontSize: 20,
+        fontVariationSettings: '"wght" 600'
+    },
+    button: {
+        borderRadius: '10px',
+        backgroundColor: 'var(--primary-fill)',
+        border: 'none',
+        padding: '7px',
+        margin: '10px',
+    }
+}
+
+
+export const Navbar = () => {
+
+    return (
+        // <div style={sty.navbar}>
+            <nav style={sty.navbarInner}>
+                <div>
+                    <button style={sty.button}>
+                        <NavLink to={'/calc1'} style={({ isActive }) => isActive ? sty.activeNav : sty.nav}>Calc 1</NavLink>
+                    </button>
+                    <button style={sty.button}>
+                        <NavLink to={'/calc2'} style={({ isActive }) => isActive ? sty.activeNav : sty.nav}>Calc 2</NavLink>
+                    </button>
+                </div>
+            </nav>
+        // </div>
+    )
+}
